c11895dfa487bd43cf47db8a622bbf9ebf9ab925
Titov Aleksandr <t.sander.v@gmail.com>
2023-05-02 15:14:05 +0300

    feat: Add IPv4 subnets ID to output

--- outputs.tf
+++ outputs.tf
@@ -14,11 +14,21 @@ output "vpc_cidr" {
 }
 
 output "private_subnets_ids" {
-  description = "List of IDs of Private Subnets"
+  description = "List of IDs of Private Subnets in UUID format"
   value       = huaweicloud_vpc_subnet.private.*.id
 }
 
+output "private_subnets_subnet_ids" {
+  description = "List of IDs of the IPv4 subnet (Native OpenStack API)"
+  value       = huaweicloud_vpc_subnet.private.*.subnet_id
+}
+
 output "public_subnets_ids" {
-  description = "List of IDs of Public Subnets"
+  description = "List of IDs of Public Subnets in UUID format"
   value       = huaweicloud_vpc_subnet.public.*.id
+}
+
+output "public_subnets_subnet_ids" {
+  description = "List of IDs of the IPv4 subnet (Native OpenStack API)"
+  value       = huaweicloud_vpc_subnet.public.*.subnet_id
 }
